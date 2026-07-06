7703c57d6d213b4e07a4ffe8cbbe7829951095e3
Rushil Patel <rushil13579@gmail.com>
2026-07-06 14:22:13 +0530

    init

--- BreadcrumbOption.ts
+++
@@ -1,9 +0,0 @@
-export class BreadcrumbOption extends BaseOptionComponent {
-    static id = "breadcrumb_option";
-    static template = "website.BreadcrumbOption";
-    static components = { BaseVisibilityOption };
-}
-
-registry.category("website-options").add(BreadcrumbOption.id, BreadcrumbOption);
-
-
--- file.js
+++
@@ -1,5 +0,0 @@
-function file () {
-    console.log("This is a file hello new.");
-}
-
-var publicName = "Sam";
